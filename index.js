7d69abd233a5a956310de67dd723542204be8aac
MD JABER HUSSAIN <mdjaberofficial@gmail.com>
2026-02-19 23:20:59 +0600

    get method added, which adds the link to get data

--- index.js
+++ index.js
@@ -28,6 +28,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffees");
+
+    //Add a new coffee to the "coffees" collection in the "coffeeDB" database
+    app.post('/addCoffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await client.db("coffeeDB").collection("coffees").insertOne(newCoffee);
+      res.send(result);
+    });
+
+    //Get a list of coffee from the "coffees" collection in the "coffeeDB" database
+    app.get('/coffees', async (req, res) => {
+        const result = await await coffeeCollection.find().toArray();
+        res.send(result);
+
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
